ffb28e1cea484bba19351aa8a21be90f94592b22
Rui Xiang <xiangruimx@163.com>
2017-07-31 17:16:04 +0800

    更新常用类

--- src/com/joker/staticcommon/StringUtility.java
+++ src/com/joker/staticcommon/StringUtility.java
@@ -399,4 +399,90 @@ public class StringUtility {
 		Double maxLng = longitude + radiusLng;
 		return new double[] { minLat, minLng, maxLat, maxLng };
 	}
+
+	/**
+	 * 插入数据库utf-8时有时会报存在 emoji 表情错误 来替换掉string 中的类似字符
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String replaceStr(String str) {
+		return str.replaceAll("[\\x{10000}-\\x{10FFFF}]", "");
+	}
+
+	/**
+	 * 将大写数字改为阿拉伯数组
+	 * 
+	 * @param chineseNumber
+	 * @return int
+	 */
+	public static String chineseToNumber(String chineseNumber) {
+		chineseNumber = chineseNumber.replace("两", "二");
+		if (chineseNumber.contains("点")) { // 对带点的数取整
+			String str1 = chineseNumber.substring(0, chineseNumber.indexOf("点"));
+			String str2 = chineseNumber.substring(chineseNumber.indexOf("点") + 1, chineseNumber.length());
+			/// return chineseNumberToInt(str1) + " پۈتۈن ئوندەن " +
+			/// chineseNumberToInt(str2);
+			return chineseNumberToInt(str1) + "." + chineseNumberToInt(str2);
+		} else {
+			return chineseNumberToInt(chineseNumber) + "";
+		}
+	}
+
+	public static Integer chineseNumberToInt(String chineseNumber) {
+		chineseNumber = chineseNumber.replace("两", "二");
+		int result = 0;
+		int temp = 1;// 存放一个单位的数字如：十万
+		int count = 0;// 判断是否有chArr
+		char[] cnArr = new char[] { '一', '二', '三', '四', '五', '六', '七', '八', '九' };
+		char[] chArr = new char[] { '十', '百', '千', '万', '亿' };
+		for (int i = 0; i < chineseNumber.length(); i++) {
+			boolean b = true;// 判断是否是chArr
+			char c = chineseNumber.charAt(i);
+			for (int j = 0; j < cnArr.length; j++) {// 非单位，即数字
+				if (c == cnArr[j]) {
+					if (0 != count) {// 添加下一个单位之前，先把上一个单位值添加到结果中
+						result += temp;
+						temp = 1;
+						count = 0;
+					}
+					// 下标+1，就是对应的值
+					temp = j + 1;
+					b = false;
+					break;
+				}
+			}
+			if (b) {// 单位{'十','百','千','万','亿'}
+				for (int j = 0; j < chArr.length; j++) {
+					if (c == chArr[j]) {
+						switch (j) {
+						case 0:
+							temp *= 10;
+							break;
+						case 1:
+							temp *= 100;
+							break;
+						case 2:
+							temp *= 1000;
+							break;
+						case 3:
+							temp *= 10000;
+							break;
+						case 4:
+							temp *= 100000000;
+							break;
+						default:
+							break;
+						}
+						count++;
+					}
+				}
+			}
+			if (i == chineseNumber.length() - 1) {// 遍历到最后一个字符
+				result += temp;
+			}
+		}
+		return result;
+	}
+
 }
